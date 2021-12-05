2a8e1f54b7fae6460095b0114fbb8198a294eedd
Nicolás Albornoz <nico.albornoz.s@gmail.com>
2021-12-05 11:50:36 -0500

    feat: Define api variables

--- website/app.js
+++ website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
-
+const api = 'api.openweathermap.org/data/2.5/forecast?id=';
+const zipcode = document.getElementById('zip').value;
+const apiKey = '&appid=9966fdbc869e3914dcbda7cb0663249e';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
